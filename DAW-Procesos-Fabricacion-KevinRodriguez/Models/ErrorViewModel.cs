6cf8603de9b4c3f5fadc3f340493ee1e616282a9
Kevin Rodriguez <kevin.rodriguez5@catolica.edu.sv>
2026-07-30 17:05:54 -0600

    proyecto base

---
+++ DAW-Procesos-Fabricacion-KevinRodriguez/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace DAW_Procesos_Fabricacion_KevinRodriguez.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
